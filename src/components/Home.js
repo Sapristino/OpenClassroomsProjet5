96d8a710727362737da37b39621b882929689d84
Sapristino <158262315+Sapristino@users.noreply.github.com>
2024-03-13 12:20:28 +0100

    Cards
    Ajout LogoNav pour logo dans barre navigateur
    Fichier annoncesData déplacé dans src
    Chgt mineur dans index.html (lang + description content + title)
    Ajout du LogoNav dans le manifest.json
    Ajout card dans Home.js
    Ajout propriété css card dans Home.scss
    
    Version desktop et mobile OK
    Prochaine étape :
    faire page annonce + routage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import annoncesData from '../data/annoncesData.json'
+
+function Card({ title, cover }) {
+  return (
+    <div className='card'>
+      <img src={cover} alt={title} className='card-image'/>
+      <div className='card-title'>{title}</div>
+    </div>
+  );
+}
 
 function Home() {
   return (
     <div className="home-container">
       <div className="image-container">
         <img src='/assets/image-accueil.png' alt='paysage accueil' className='home-image'></img>
-        <div className="overlay-text">Chez vous, partout et ailleurs</div>
+        <div className="overlay-text"><span>Chez vous,</span><span> partout et ailleurs</span></div>
       </div>
       <div className="content-container">
-        {/*logements*/}
+        {annoncesData.map(annonce => (
+          <Card key={annonce.id} title={annonce.title} cover={annonce.cover} />
+        ))}
       </div>
     </div>
   );
